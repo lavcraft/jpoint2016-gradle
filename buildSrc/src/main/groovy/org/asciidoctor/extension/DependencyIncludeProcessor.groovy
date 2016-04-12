c81f03bc5b0e138ed3b4333e9842af81c33cfb0a
Kirill Tolkachev <lavcraft@gmail.com>
2016-04-12 22:25:45 +0300

    Fix ruby gen crash

--- buildSrc/src/main/groovy/org/asciidoctor/extension/DependencyIncludeProcessor.groovy
+++ buildSrc/src/main/groovy/org/asciidoctor/extension/DependencyIncludeProcessor.groovy
@@ -1,22 +1,25 @@
 package org.asciidoctor.extension
 
-import groovy.util.logging.Slf4j
 import org.asciidoctor.ast.DocumentRuby
+
 /**
  * @author tolkv
  * @since 12/04/16
  */
-@Slf4j
 class DependencyIncludeProcessor extends IncludeProcessor {
+
+  public DependencyIncludeProcessor(Map<String, Object> config) {
+    super(config)
+  }
 
   @Override
   boolean handles(String target) {
-    log.info 'target : {}', target
-    return target.startsWith('gradle')
+    return target.startsWith('gradle://')
   }
 
   @Override
   void process(DocumentRuby document, PreprocessorReader reader, String target, Map<String, Object> attributes) {
-    reader.push_include('hello ' + System.currentTimeMillis(), target, target, 1, attributes)
+    String s = 'hello ' + System.currentTimeMillis()
+    reader.push_include(s, target, target, 1, attributes)
   }
 }

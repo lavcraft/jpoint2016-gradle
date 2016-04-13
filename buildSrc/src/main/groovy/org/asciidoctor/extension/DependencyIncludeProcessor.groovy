821d678ea40c8272f7ef345c6eb714ee6ce99f3f
Kirill Tolkachev <lavcraft@gmail.com>
2016-04-13 06:41:00 +0400

    Add include processer content read from zip

--- buildSrc/src/main/groovy/org/asciidoctor/extension/DependencyIncludeProcessor.groovy
+++ buildSrc/src/main/groovy/org/asciidoctor/extension/DependencyIncludeProcessor.groovy
@@ -1,12 +1,15 @@
 package org.asciidoctor.extension
 
 import org.asciidoctor.ast.DocumentRuby
+import org.gradle.api.Project
 
 /**
  * @author tolkv
  * @since 12/04/16
  */
 class DependencyIncludeProcessor extends IncludeProcessor {
+  //TODO WTF .. we need to avoid static project set... However, maybe it is right way :)
+  static Project project
 
   public DependencyIncludeProcessor(Map<String, Object> config) {
     super(config)
@@ -19,7 +22,22 @@ class DependencyIncludeProcessor extends IncludeProcessor {
 
   @Override
   void process(DocumentRuby document, PreprocessorReader reader, String target, Map<String, Object> attributes) {
-    String s = 'hello ' + System.currentTimeMillis()
+
+    def dependencies = project.configurations.docs.collect {
+      project.zipTree(it).collect {
+        it.readLines()
+      }
+    }
+
+    String s = """
+    time:            ${System.currentTimeMillis()}
+    project:         ${project?.name}
+    document:        $document
+    target:          $target
+    attributes:      $attributes
+    dependencies:    $dependencies
+    """
+
     reader.push_include(s, target, target, 1, attributes)
   }
 }
